27c88d2cfc4683aa22bd2da727377d01bd1a2f39
Andreas Linden <andreas.linden@coxautoinc.com>
2021-09-13 09:20:48 +0200

    add info

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const core = require('@actions/core');
 const processIncludes = fileName => {
     try {
         const currentPath = path.dirname(fileName);
-        core.info("current path: " + currentPath);
-        core.info("read file: " + fileName);
-
         const fileContent = fs.readFileSync(fileName, 'utf8');
+
+        core.info("processing file: " + fileName);
 
         return fileContent.replace(/^#include "([^"]+)"/m, (match, includeFile) => {
             const realPath = path.resolve(currentPath + '/' + includeFile);
+            core.info('#include "'+ includeFile +'"');
             return processIncludes(realPath);
         });
 
@@ -25,4 +25,8 @@ const inputFile = core.getInput('input');
 const outputFile = core.getInput('output');
 
 const processed = processIncludes(inputFile);
+
+core.info("Writing processed file contents to: "+ outputFile);
+core.info(processed);
+
 fs.writeFileSync(outputFile, processed);
