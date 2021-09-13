39f14a5d8b2b62405330e8a19626b869b9523caa
Andreas Linden <andreas.linden@coxautoinc.com>
2021-09-13 08:35:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const core = require('@actions/core');
+
+const processIncludes = fileName => {
+    try {
+        const currentPath = path.dirname(fileName);
+        const fileContent = fs.readFileSync(fileName, 'utf8');
+
+        return fileContent.replace(/^#include "([^"]+)"/m, (match, includeFile) => {
+            const realPath = path.resolve(currentPath + '/' + includeFile);
+            return processIncludes(realPath);
+        });
+
+    } catch (err) {
+        process.stderr.write(err);
+        process.exit(1);
+    }
+};
+
+const inputFile = core.getInput('input');
+const outputFile = core.getInput('output');
+
+const processed = processIncludes(inputFile);
+fs.writeFileSync(outputFile, processed);
